7a40c705983f020af80171da83b907227ff9c990
skhomuti <skhomuti@gmail.com>
2018-01-15 21:04:35 +0500

    RF: Added doctests

--- allure-robotframework/src/utils.py
+++ allure-robotframework/src/utils.py
@@ -27,12 +27,23 @@ def get_allure_parameters(parameters):
 
 
 def get_allure_suites(longname):
+    """
+    >>> get_allure_suites('Suite1.Test')
+    [Label(name='suite', value='Suite1')]
+    >>> get_allure_suites('Suite1.Suite2.Test')
+    [Label(name='suite', value='Suite1'), Label(name='subSuite', value='Suite2')]
+    >>> get_allure_suites('Suite1.Suite2.Suite3.Test')  # doctest: +NORMALIZE_WHITESPACE
+    [Label(name='parentSuite', value='Suite1'),
+    Label(name='suite', value='Suite2'),
+    Label(name='subSuite', value='Suite3')]
+    """
     labels = []
     suites = longname.split('.')
     if len(suites) > 3:
         labels.append(Label('parentSuite', suites.pop(0)))
-    labels.extend([Label('suite', suites.pop(0)),
-                   Label('subSuite', '.'.join(suites[:-1]))])
+    labels.append(Label('suite', suites.pop(0)))
+    if len(suites) > 1:
+        labels.append(Label('subSuite', '.'.join(suites[:-1])))
     return labels
 
 
